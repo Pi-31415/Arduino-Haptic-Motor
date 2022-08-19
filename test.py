c8e994d722030e4844a47a37e1fe41dd7088a948
Pi <paingthetko.pi@gmail.com>
2022-08-19 15:59:44 +0400

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,34 @@
 import serial.tools.list_ports
 import time
 import serial
+import socket
+
+
+localIP = "127.0.0.1"
+localPort = 5500
+bufferSize = 1024
+
+msgFromServer = "Hello UDP Client"
+bytesToSend = str.encode(msgFromServer)
+
+# Arduino Setup
+
+
 ports = list(serial.tools.list_ports.comports())
+arduino_port_name = ""
 for p in ports:
-    print(p)
+    if("Arduino" in str(p)):
+        arduino_port_name = str(p).split(" -")[0]
+
+# If Arduino is not found, don't do any actions
+if(arduino_port_name == ""):
+    print("\n\nError : Haptic Glove not found. Please connect.")
+else:
+    print("\n\nConnected to Haptic Glove on port "+arduino_port_name)
 
 
 # Importing Libraries
-arduino = serial.Serial(port='/dev/cu.usbmodem144301',
+arduino = serial.Serial(port=arduino_port_name,
                         baudrate=115200, timeout=.1)
 
 
